d38c82eaff4f676f182286e0f952a43fb5f67f7f
Koen Klinkers <k.klinkers@gmail.com>
2024-03-01 22:16:50 +0100

    update cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressStyle};
 use rand::distributions::{Alphanumeric, DistString};
 use reqwest::header;
 use serde::{Deserialize, Serialize};
@@ -36,13 +36,12 @@ impl ImgRequest<'_> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let prompt: String = env::args().collect::<Vec<_>>()[1..].join(" ");
     let key = env::var("OPENAI_API_KEY".to_string()).expect("OPENAI_API_KEY not set");
-    let m = MultiProgress::new();
 
-    let sty = ProgressStyle::with_template("[{elapsed_precise:.green}] {msg}")?;
+    let sty = ProgressStyle::with_template("{spinner:.green} {msg}")?;
 
-    let pb = m.add(ProgressBar::new_spinner());
+    let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(Duration::from_millis(120));
-    pb.set_style(sty.clone());
+    pb.set_style(sty);
 
     pb.set_message("🤯 Generating");
     let mut headers = header::HeaderMap::new();
@@ -55,21 +54,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json(&ImgRequest::new(&prompt))
         .send()?;
     let payload: Payload = res.json()?;
-    pb.finish_with_message("Generated");
-
-    let pb2 = m.add(ProgressBar::new_spinner());
-    pb2.enable_steady_tick(Duration::from_millis(120));
-    pb2.set_style(sty.clone());
-
-    pb2.set_message("💻 Downloading");
     let url = &payload.data[0].url;
 
+    pb.set_message("💻 Downloading");
     let prefix = Alphanumeric.sample_string(&mut rand::thread_rng(), 5);
     let path = format!("./{}-{}.png", prefix, prompt.replace(" ", "-"));
     let mut file = std::fs::File::create(&path)?;
     client.get(url).send()?.copy_to(&mut file)?;
-    pb2.finish_with_message("Downloaded");
 
-    println!("{}", path);
+    pb.finish_with_message(format!(" 🎉 {}", path));
+
     Ok(())
 }
