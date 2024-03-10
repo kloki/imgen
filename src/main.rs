afcc741d5a3391d3f38453ef8ef6a62b0bdf7ea7
Koen Klinkers <k.klinkers@gmail.com>
2024-03-10 15:23:16 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use indicatif::{ProgressBar, ProgressStyle};
 use rand::distributions::{Alphanumeric, DistString};
+use reqwest::blocking::Client;
 use reqwest::header;
+use reqwest::header::HeaderMap;
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::time::Duration;
@@ -33,6 +35,38 @@ impl ImgRequest<'_> {
         }
     }
 }
+
+fn build_filename(prompt: &str) -> String {
+    let prefix = Alphanumeric.sample_string(&mut rand::thread_rng(), 5);
+    let mut name = prompt.replace(" ", "-").replace(".", "");
+    name.truncate(60);
+    format!("./{}-{}.png", prefix, name)
+}
+
+fn process_prompt(
+    client: Client,
+    prompt: String,
+    pb: ProgressBar,
+    headers: HeaderMap,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let res = client
+        .post("https://api.openai.com/v1/images/generations")
+        .headers(headers)
+        .json(&ImgRequest::new(&prompt))
+        .send()?;
+    let payload: Payload = res.json()?;
+    let url = &payload.data[0].url;
+
+    pb.set_message("💻 Downloading");
+    let path = build_filename(&prompt);
+    let mut file = std::fs::File::create(&path)?;
+    client.get(url).send()?.copy_to(&mut file)?;
+
+    pb.finish_with_message(format!(" 🎉 {}", path));
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let prompt: String = env::args().collect::<Vec<_>>()[1..].join(" ");
     let key = env::var("OPENAI_API_KEY".to_string()).expect("OPENAI_API_KEY not set");
@@ -48,23 +82,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     headers.insert("Content-Type", "application/json".parse()?);
     headers.insert("Authorization", ["Bearer ", &key].concat().parse()?);
     let client = reqwest::blocking::Client::new();
-    let res = client
-        .post("https://api.openai.com/v1/images/generations")
-        .headers(headers)
-        .json(&ImgRequest::new(&prompt))
-        .send()?;
-    let payload: Payload = res.json()?;
-    let url = &payload.data[0].url;
-
-    pb.set_message("💻 Downloading");
-    let prefix = Alphanumeric.sample_string(&mut rand::thread_rng(), 5);
-    let mut name = prompt.replace(" ", "-").replace(".", "");
-    name.truncate(60);
-    let path = format!("./{}-{}.png", prefix, name);
-    let mut file = std::fs::File::create(&path)?;
-    client.get(url).send()?.copy_to(&mut file)?;
-
-    pb.finish_with_message(format!(" 🎉 {}", path));
+    process_prompt(client, prompt, pb, headers)?;
 
     Ok(())
 }
